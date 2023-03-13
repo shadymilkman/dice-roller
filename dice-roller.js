c1fef7c71c413a9b0a07ff077599071555d7c69a
shadymilkman <dburrow13@gmail.com>
2023-03-12 21:29:56 -0500

    initial commit

---
+++ dice-roller.js
@@ -0,0 +1,118 @@
+
+// // Define Game type
+// const gameType = [
+//   'vampire',
+// ]
+
+// // Define empty array to store dice roll pass/fail threshold
+// let diceRollPassFail = [];
+
+// // Dfine pass/fail threshold based on gameTeyp (if required)
+// if (gameType === 'vampire') {
+//   diceRollPassFail = 6;
+// }
+
+// Prompt the user to enter the number of, and which kind of dice to roll
+let argRollDice = prompt('Enter your dice roll (Ex: 5d10):');
+
+let argDiceInputSplit = argRollDice.split('d');
+
+// Define the argment for number of dice by (extracted from input)
+let argNumberOfDice = Number(argDiceInputSplit[0]);
+
+// Define the type of dice to roll (extracted from input)
+let argTypeOfDice = Number(argDiceInputSplit[1]);
+
+// Define loop counter
+let loopCount = 0;
+
+let playerDiceRollsArray = [];
+
+let playerRollsHardFails = [];
+
+let playerRollsTensReroll = [];
+
+// let rollDiceArray = [];
+
+// While loopCount is less than argNumberOfDice
+// while (loopCount < argNumberOfDice) {
+//   // Generate random number between 1 and x based on number returned by argTypeOfDice
+//   // currentDiceRoll = Math.floor(Math.random() * argTypeOfDice) + 1;
+//   playerDiceRollsArray.push(Math.floor(Math.random() * argTypeOfDice) + 1);
+//   // Math.floor(Math.random() * argTypeOfDice) + 1;
+//   // Increment loopCount
+//   loopCount++;
+// }
+
+// Generates a random number between 1 and n based on the number of dice, and type specified
+function rollDice(rollDiceNumber, rollDiceType) {
+
+  // Define and empty array
+  let rollDiceArray = [];
+
+  // While 'rollDiceNumber' is greater than '0'
+  while (rollDiceNumber > 0) {
+
+    // Assign 'rollDiceResult' the result of a random number between 1 and n based on the rollDiceType parameter plus 1
+    const rollDiceResult = Math.floor(Math.random() * rollDiceType) + 1;
+
+    // Pushes the rusult assigned to 'rollDiceResult' to 'rollDiceArray'
+    rollDiceArray.push(rollDiceResult);
+
+    // Subtracts one from 'rollDiceNumber' to iterate the loop
+    rollDiceNumber--;
+  }
+
+  // Returns the values in the 'rollDiceArray'
+  return rollDiceArray;
+}
+
+playerDiceRollsArray.push(rollDice(argNumberOfDice, argTypeOfDice));
+// playerDiceRollsArray = rollDice(argNumberOfDice, argTypeOfDice, loopCount);
+
+console.log(playerDiceRollsArray);
+// modified playerDiceRolls
+
+
+
+// for (const [index, playerDiceRoll] of playerDiceRollsArray.entries()) {
+//   if (playerDiceRoll === 1) {
+//     playerRollsHardFails.push(playerDiceRoll);
+//     console.log(`${index + 1}d${argNumberOfDice} = ${playerDiceRoll} - *HARD FAIL*`);
+//   }
+//   else if (playerDiceRoll < 6) {
+//     console.log(`${index + 1}d${argNumberOfDice} = ${playerDiceRoll} - *FAIL*`);
+//   }
+//   else if (playerDiceRoll === 10) {
+//     playerRollsTensReroll.push(playerDiceRoll);
+//     console.log(`${index + 1}d${argNumberOfDice} = ${playerDiceRoll} - *REROLL 10*`);
+
+//   }
+//   else {
+//     console.log(`${index + 1}d${argNumberOfDice} = ${playerDiceRoll} - *PASS*`);
+//   }
+// }
+
+// Define pass/fail for D10 based 'Vampire: The Masquerade' game
+// for (const [index, playerDiceRoll] of playerDiceRollsArray.entries()) {
+//   if (playerDiceRoll < 6) {
+//     console.log(`${index + 1}d${argTypeOfDice} ${playerDiceRoll} - FAIL`);
+//   }
+//   else {
+//     console.log(`${index + 1}d${argTypeOfDice} ${playerDiceRoll} - PASS`);
+//   }
+// }
+// console.log(typeof diceRollPassFail);playerDiceRollsArray.filter(playerDiceRoll => playerDiceRoll !== 1)
+
+//  if (playerDiceRoll === 1) {
+//   playerRollSubtractiveFails.push(playerDiceRoll);
+//   playerDiceRollsArray.push(playerDiceRoll);
+// }
+// else if (playerDiceRoll === 10) {
+//   playerRollTensReroll.push(playerDiceRoll);
+//   playerDiceRollsArray.push(playerDiceRoll);
+// }
+// else {
+//   playerDiceRollsArray.push(playerDiceRoll);
+// }
+// (`${index + 1}d${argNumberOfDice} = ${playerDiceRoll} - *HARD FAIL*`)
